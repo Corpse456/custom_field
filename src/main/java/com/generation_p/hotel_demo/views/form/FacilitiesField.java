80d46b1d4efdb73ecc3721e54cfd05c948d37f9d
Alexander Neznaev <Corpsetrup@gmail.com>
2018-06-08 08:40:20 +0200

    Done

--- src/main/java/com/generation_p/hotel_demo/views/form/FacilitiesField.java
+++ src/main/java/com/generation_p/hotel_demo/views/form/FacilitiesField.java
@@ -2,18 +2,16 @@ package com.generation_p.hotel_demo.views.form;
 
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-import org.openqa.selenium.WebElement;
-
 import com.vaadin.icons.VaadinIcons;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.CustomField;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.NativeButton;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 
@@ -22,8 +20,6 @@ public class FacilitiesField extends CustomField<String> {
 
     private Set<String> facilities = new TreeSet<>();
     private String caption = "Facilities";
-    private NativeButton del = new NativeButton();
-    private Label label = new Label("Fuck");
     private VerticalLayout layout = new VerticalLayout();
     
     @Override
@@ -40,24 +36,72 @@ public class FacilitiesField extends CustomField<String> {
     @Override
     protected Component initContent () {
         super.setCaption(caption);
-        
-        /*del.setStyleName(ValoTheme.BUTTON_SMALL);
-        del.setStyleName(ValoTheme.BUTTON_BORDERLESS_COLORED);
-        del.setWidth("100%");
-        del.setIcon(VaadinIcons.CLOSE_CIRCLE_O);
-        
-        label.setWidth("100%");
-        layout.setWidth("100%");
-        
         layout.setMargin(false);
         
-        layout.addComponents(label, del);*/
+        HorizontalLayout adding = new HorizontalLayout();
+        Button add = new Button("Add");
+        TextField input = new TextField();
+        
+        input.setPlaceholder("Input facility");
+        input.addValueChangeListener(l -> {
+            add.setEnabled(!input.isEmpty());
+        });
+        input.setWidth("100%");
+        
+        add.setEnabled(false);
+        add.addClickListener(l -> {
+            facilities.add(input.getValue());
+            add.setEnabled(false);
+            input.clear();
+            doSetValue(getValue());
+        });
+        add.setWidth("100%");
+        
+        adding.addComponents(input, add);
+        adding.setWidth("100%");
+        layout.addComponent(adding);
+        layout.setWidth("100%");
         return layout;
     }
 
     @Override
     protected void doSetValue (String value) {
-        Arrays.asList(value.split(",")).forEach(val -> facilities.add(val));;
-        //label.setValue(facilities);
+        layout.removeAllComponents();
+        initContent();
+        facilities.clear();
+        
+        if (value != null) Arrays.asList(value.split(",")).forEach(val -> facilities.add(val));
+        else return;
+        
+        Iterator<String> iterator = facilities.iterator();
+        while (iterator.hasNext()) {
+            currentFaclilityBuilder(iterator.next());
+        }
+    }
+
+    private void currentFaclilityBuilder (String facility) {
+        HorizontalLayout current = new HorizontalLayout();
+        
+        NativeButton currentCloseButton = new NativeButton();
+        currentCloseButton.setStyleName(ValoTheme.BUTTON_SMALL);
+        currentCloseButton.setStyleName(ValoTheme.BUTTON_BORDERLESS);
+        currentCloseButton.setWidth("100%");
+        currentCloseButton.setIcon(VaadinIcons.CLOSE_CIRCLE_O);
+        currentCloseButton.addClickListener(l -> {
+           facilities.remove(facility);
+           layout.removeComponent(current);
+        });
+        
+        NativeButton currentLabel = new NativeButton(facility);
+        currentLabel.setStyleName(ValoTheme.BUTTON_SMALL);
+        currentLabel.setStyleName(ValoTheme.BUTTON_BORDERLESS);
+        currentLabel.setWidth("100%");
+        currentLabel.setEnabled(false);
+        
+        current.setWidth("100%");
+        current.setMargin(false);
+        current.addComponents(currentLabel, currentCloseButton);
+        
+        layout.addComponent(current);
     }
 }
